3e0da690aac186975068ca5f9c756dfb7583ddca
Patrick Kolodziejczyk <pk@devmars.fr>
2015-06-14 09:24:20 +0200

    Add the script that fix the UI for display all active  properly

---
+++ fix_ui.js
@@ -0,0 +1,3 @@
+$J('.tv_ui').css('background-image','url("https://raw.githubusercontent.com/pkolodziejczyk/SteamMonsterGameScript/master/game_frame_tv_fix.png")');
+$J('#activeinlanecontainer').css('height','134px');
+$J('#activitycontainer').css('height', '360px');
